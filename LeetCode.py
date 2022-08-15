aff6ca17f238eaec05ced3277a18ba6d23c20493
MarinoLinic <84214511+MarinoLinic@users.noreply.github.com>
2022-08-15 21:19:50 +0200

    Roman to Integer

--- LeetCode.py
+++ LeetCode.py
@@ -14,3 +14,36 @@ class Solution:
                     return [hashmap[remainder], i]
             
             hashmap[nums[i]] = i
+
+
+# Roman to Integer
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        roman_numerals = {
+            'I': 1, 
+            'V': 5, 
+            'X': 10, 
+            'L': 50, 
+            'C': 100, 
+            'D': 500, 
+            'M': 1000
+        }
+        
+        n = len(s)
+        answer = 0
+        
+        for i in range(n):
+            
+            # Avoid index out of range
+            if i == n-1:
+                answer += roman_numerals[s[i]]
+                break
+            
+            # Check if the character at right of current character is bigger or smaller
+            if roman_numerals[s[i]] >= roman_numerals[s[i+1]]:
+                answer += roman_numerals[s[i]]
+            else:
+                answer -= roman_numerals[s[i]]
+                
+        return answer
+        
